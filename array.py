5fc39c4ae9db633f0cac517d653bfcd1b799a83a
Sayo Everden <syeverden@gmail.com>
2021-09-18 10:24:46 -0700

    change for the A1

--- array.py
+++ array.py
@@ -1,3 +1,7 @@
 sortable_array = [4,"s",3,"a",7, 9,"y","o"]
 
 print(sortable_array)
+int_list = [x for x in sortable_array if isinstance(x,  int)]
+str_list = [x for x in sortable_array if isinstance(x,  str)]
+int_list.sort()
+print(str_list)
