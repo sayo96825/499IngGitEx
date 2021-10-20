a8d798d016f52b0601256a1d01c7648ef38fc630
Sayo Everden <syeverden@gmail.com>
2021-10-20 10:42:42 -0700

    making first chage to array

--- array.py
+++ array.py
@@ -3,5 +3,6 @@ sortable_array = [4,"s",3,"a",7, 9,"y","o"]
 print(sortable_array)
 int_list = [x for x in sortable_array if isinstance(x,  int)]
 str_list = [x for x in sortable_array if isinstance(x,  str)]
+#making change
 int_list.sort()
 print(str_list)
